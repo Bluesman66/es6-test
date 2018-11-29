26a515b236e457ca725b42b16cdde3e23df01dcb
alex.paclin <alex.paclin@gmail.com>
2018-11-29 15:51:53 +0500

    calculator

---
+++ src/js/calculator.js
@@ -0,0 +1,13 @@
+var calculator = {
+    sum: function (a, b) {
+        return a + b;
+    },
+    dif: function (a, b) {
+        return a - b;
+    }
+}
+
+const a = 4;
+
+export default calculator;
+export {a};
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import calculator, {a} from './calculator';
+
+const b = 2;
+
+const sum = calculator.sum(a, b);
+console.log(sum);
